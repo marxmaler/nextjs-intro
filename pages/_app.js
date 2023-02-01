9e09c6cbde57fb63ba811b7619a70ce43dc03d97
Sabi <84113149+marxmaler@users.noreply.github.com>
2023-02-01 23:47:20 +0900

    course ended

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Layout from "@/components/Layout";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  //여기서 pageProps는 해당 page component가 getServerSideProps로부터 반환받은 props
   return (
     <Layout>
       <Component {...pageProps}></Component>
